6e450ce242f2264e69869bfb033eb14677ba0bb3
Ivan Sangermano <ivan.j.sanger@gmail.com>
2021-11-23 21:34:35 -0300

    footer added

---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <div className={styles.header}>
+        <footer>          
+            <p>
+                Parcial Nº2 Modelo computacional de Gestion Empresarial - Ivan Sangermano
+            </p>
+        </footer>
+    </div>
+  );
+};
--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Header } from './Header';
 import { NavBar } from './NavBar';
+import {Footer} from './Footer'
 import styles from './Layout.module.css';
 
 export const Layout = ({ children }) => {
   return (
     <div className={styles.mainWrapper}>
       <NavBar />
-      <div className={styles.content}>
-        <Header title="UserApp" />
-        {children}
+      <div className={styles.body}>
+        <div className={styles.content}>
+          <Header title="UserApp" />
+          {children}
+        </div>
+        <div className={styles.foot}>
+          <Footer/>
+        </div>
       </div>
     </div>
   );
